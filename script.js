fe9aa7bce6e0b3f3313612c9cc86573af5baea7c
sreejith <sreejithsree6537@gmail.com>
2025-10-06 00:50:59 +0530

    Updated code after recent changes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const hamburger = document.querySelector('.hamburger-btn');
 const navMenu = document.querySelector('.nav-menu');
 
+const formEl = document.querySelector(".form-div");
+
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const msg = document.getElementById("message");
+const sbtBtn = document.querySelector('.msg-send');
+
+formEl.addEventListener("submit",()=>{
+  const inputName = username.value;
+  const userEmail = email.value;
+  const message = msg.value;
+
+  if(inputName == ""){
+    alert("Please Enter Your Name")
+  }else if(userEmail == ""){
+    alert("Please Enter Your Email")
+  }else if(message.value == ""){
+    alert("Please Enter Description Text")
+  }
+})
 
 
 hamburger.addEventListener("click",()=>{
